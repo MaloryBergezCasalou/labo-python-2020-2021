dd3d9d58b16ba96a25e0690c3437fb18d786683a
malorybergezcasalou <malory.bergezcasalou@gmail.com>
2020-11-25 12:15:31 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # labo-python-2020-2021
+
+What I do in the python lab in 2020-2021
---
+++ encryption/script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# malorybergezcasalou
+# script encryption passwd
+
+from cryptography.fernet import Fernet
+
+typePass = input("type password: ")
+
+def encryption():
+    encryptPass = 
+    
+# fichier = open("passEncrypt.txt", "x")
